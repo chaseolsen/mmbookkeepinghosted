d7cc47c03222e91581c029b7dda3fbc5e8b85ec4
Chase Olsen <chase.olsen22@gmail.com>
2016-12-05 16:12:27 -0700

    Revamped mobile view. Completely responsive and dropdown now appears

--- public/styles/jquery.js
+++ public/styles/jquery.js
@@ -7,14 +7,3 @@ $(document).ready(function(){
   setTimeout(function(){
     $('.fadeThree').fadeIn(2500);
   }, 1000);
-
-
-  $scope.login = function(username, password){
-    mainSvc.login({
-      //first username taking values from function parameters
-      username: username,
-      password: password
-    }).then(function(response){
-      console.log(response.data);
-    });
-  };
